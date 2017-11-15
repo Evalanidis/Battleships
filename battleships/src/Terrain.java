c63ceb9828e91644b9cefc38ee58f749bcfea53f
Efstathios Valanidis <evalanidis@gmail.com>
2017-11-15 17:10:14 +0200

    Update Terrain.java

--- battleships/src/Terrain.java
+++ battleships/src/Terrain.java
@@ -6,27 +6,30 @@
 
 public class Terrain {
 
-    public static char [][] battleTerrain =new char [10][10];
+    public static char [][] battleTerrainPlayer =new char [10][10];
+    public static char [][] battleTerrainPC =new char [10][10];
 
     public void Terrain(){
-        createTerrain();
-        printTerrain();
+        createTerrain(battleTerrainPlayer);
+        createTerrain(battleTerrainPC);
+        printTerrain(battleTerrainPlayer);
+        createTerrain(battleTerrainPC);
 
     }
 
 
-    public void createTerrain(){
+    public void createTerrain(char[][] battleTerrain){
         for(int i = 0; i<10;i++)
             for (int j =0; j<10;j++)
                 battleTerrain[i][j] = '.';
     }
 
     public void terrainPreparation(int i, int j){
-        battleTerrain[i][j] = '@';
+        battleTerrainPlayer[i][j] = '1';
     }
 
     public boolean freeSpace(int i,int j){
-        return battleTerrain[i][j]=='.';
+        return battleTerrainPlayer[i][j]=='.';
     }
 
     public void printTerrain(){
@@ -40,10 +43,28 @@ public class Terrain {
         for(int i = 0; i<10;i++){
             System.out.print(i+1+ "\t");
             for (int j =0; j<10;j++){
-                System.out.print(battleTerrain[i][j] +"  ");
+                System.out.print(battleTerrainPlayer[i][j] +"  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printTerrain(char [][] currentTerrain){
+        for(int x = 0 ; x<10; x++) {
+            if (x == 0)
+                System.out.print("\t" + (x + 1) +"  ");
+            else
+                System.out.print((x + 1)+ "  ");
+        }
+        System.out.println();
+        for(int i = 0; i<10;i++){
+            System.out.print(i+1+ "\t");
+            for (int j =0; j<10;j++){
+                System.out.print(currentTerrain[i][j] +"  ");
             }
             System.out.println();
         }
     }
 
 }
+
